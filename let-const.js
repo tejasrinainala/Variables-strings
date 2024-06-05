2534d9ca85c2902708512d2532fafafcebce9249
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-06-05 12:45:09 +0530

    Create let-const.js

---
+++ let-const.js
@@ -0,0 +1,11 @@
+let num=15;
+num=20
+console.log(num)
+const name="Sweety"
+//name="Teja"              //returns error because const variables cannot be changed
+console.log(name) 
+{
+    let price=10;           //block scope
+    console.log(price)
+}
+
