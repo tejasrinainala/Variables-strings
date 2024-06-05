85105399677652505fcb05739223148c936e8514
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-06-05 12:51:23 +0530

    Create template-literals.js

---
+++ template-literals.js
@@ -0,0 +1,5 @@
+const name = 'Tejasri';
+const age = 19;
+const greeting = `Hello, my name is ${name} and I am ${age} years old.`;    //template literals
+
+console.log(greeting); // Output: Hello, my name is Tejasri and I am 19 years old.
